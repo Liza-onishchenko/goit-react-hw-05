383cdd1e0a013467e00e4faa4bb26a092eef174c
Liza <onishenkolizka99@gmail.com>
2024-10-28 12:59:14 +0200

    ract-05

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -56,7 +56,7 @@ const MovieDetailsPage = () => {
   const defaultImg =
     "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
-  const backUrl = useRef(location.state?.form || "/movies"); //повернення назад
+  const backUrl = useRef(location.state?.from || "/movies"); //повернення назад
 
   if (!movieData) return <div>No movie data available.</div>;
   return (
